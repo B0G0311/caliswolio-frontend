33fa805d24ec0b4004e82bf78bb95269b5f7e37b
sadavi <sadavis12@my.waketech.edu>
2023-02-17 23:19:35 -0500

    Added dropdown for Exercise Items

--- src/components/exerciseItem.jsx
+++ src/components/exerciseItem.jsx
@@ -1,28 +1,48 @@
-import React from "react";
-import { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import WorkoutContext from '../context/workoutContext';
+
 function ExerciseItem() {
+  const { selectedExercises, exerciseListIsLoaded, setActivePage } = useContext(WorkoutContext);
+  const [openDropdown, setOpenDropdown] = useState(null);
 
-    const {selectedExercises, exerciseListIsLoaded} = useContext(WorkoutContext)
+  function toggleDropdown(exerciseId) {
+    if (openDropdown === exerciseId) {
+      setOpenDropdown(null);
+    } else {
+      setOpenDropdown(exerciseId);
+    }
+  }
 
-    if (exerciseListIsLoaded) {
-        return (
-            <div>
-            {selectedExercises.exercises.map((exercise) => {
-                return (
-                <div key={exercise.exercise_id} className='exerciseItem'>
-                    <div className="exerciseDescription">{exercise.description}</div>
-                    <div className="exerciseName">Exercise: {exercise.name}</div>
-                    <div className="exerciseSets">Sets: {exercise.sets}</div>
-                    <div className="exerciseReps">Reps: {exercise.reps}</div>
+  if (exerciseListIsLoaded) {
+    return (
+      <div>
+        {selectedExercises.exercises.map((exercise) => {
+          const isOpen = openDropdown === exercise.exercise_id;
+
+          return (
+            <div key={exercise.exercise_id} className='exerciseItem'>
+              <button className='exerciseName' onClick={(e) => {
+                e.preventDefault()
+                toggleDropdown(exercise.exercise_id)
+                setActivePage('ExerciseList')
+                }}>
+                Exercise: {exercise.name}
+              </button>
+              {isOpen && (
+                <div id={`dropdown_content_${exercise.exercise_id}`} className='dropdown_content'>
+                  <a className='exerciseSets'>Sets: {exercise.sets}</a>
+                  <a className='exerciseReps'>Reps: {exercise.reps}</a>
+                  <a className='exerciseDescription'>{exercise.description}</a>
                 </div>
-            )})}
+              )}
             </div>
-        )}
-        else {
-            return null
-        }
-    } 
-    
-    
-export default ExerciseItem
+          );
+        })}
+      </div>
+    );
+  } else {
+    return null;
+  }
+}
+
+export default ExerciseItem;
