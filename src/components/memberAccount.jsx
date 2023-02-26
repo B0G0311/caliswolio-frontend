a666ead038011cfc7cda6ac9b778d6ed34235370
OnlyITY <83495117+OnlyITY@users.noreply.github.com>
2023-02-26 12:45:17 -0500

    Landing page for members

---
+++ src/components/memberAccount.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { useContext } from 'react';
+import WorkoutContext from '../context/workoutContext';
+
+ function MemberAccount()
+{
+    const { setActivePage } = useContext(WorkoutContext)
+
+    return(        
+        <div className='btn-group'>
+            <h1>Member Account</h1>
+            <br/><br/>
+
+            <button onClick={() => setActivePage('')} className='startNew' id='newWorkout'>Start New Workout</button>
+            <br/><br/>
+
+            <button onClick={() => setActivePage('')} className='workoutLog' id='checkWL'>Workout Log</button>
+            <br/><br/> 
+
+            <button onClick={() => setActivePage('')} className='workoutTemplate' id='checkWW'>Workout Templates</button>
+            <br/><br/>
+
+            <button onClick={() => setActivePage('')} className='Future Workout Queue' id='checkFWQ'>Future Workout Queue</button>
+        </div>
+    )
+}
+
+export default MemberAccount;
+
+/*
+    Log out button to be incorporated
+    <button onClick={() => setActivePage('')} className='logOut' id='logOffAcc'>Log Off</button>
+*/
