13f9fc6fae537452deb0baa68f14559f6d6b1ba7
mtmears <mmears9795@gmail.com>
2023-03-04 09:18:48 -0500

    I think I fixed the merge

--- src/components/exerciseItem.jsx
+++ src/components/exerciseItem.jsx
@@ -3,7 +3,7 @@ import WorkoutContext from '../context/workoutContext';
 import '../css/exerciseItem.css';
 
 function ExerciseItem() {
-  const { selectedExercises, exerciseListIsLoaded, setActivePage } = useContext(WorkoutContext);
+  const { selectedExercises, setSelectedExercises, exerciseListIsLoaded, setActivePage } = useContext(WorkoutContext);
   const [openDropdown, setOpenDropdown] = useState(null);
 
   function toggleDropdown(exerciseId) {
@@ -12,6 +12,32 @@ function ExerciseItem() {
     } else {
       setOpenDropdown(exerciseId);
     }
+  }
+
+  function changeReps(exerciseID, num) {
+    let newObject = selectedExercises.exercises
+    newObject.forEach(exercise => {
+      if (exercise.exercise_id === exerciseID) {
+        exercise.reps += num
+      }
+    })
+
+    setSelectedExercises({
+      'exercises': newObject
+    })
+  }
+
+  function changeSets(exerciseID, num) {
+    let newObject = selectedExercises.exercises
+    newObject.forEach(exercise => {
+      if (exercise.exercise_id === exerciseID) {
+        exercise.sets += num
+      }
+    })
+
+    setSelectedExercises({
+      'exercises': newObject
+    })
   }
 
   if (exerciseListIsLoaded) {
@@ -33,14 +59,30 @@ function ExerciseItem() {
                 <div id={`dropdown_content_${exercise.exercise_id}`} className='dropdown_content'>
                   <div className='exerciseEdit'>
                     <p className='exerciseSets'>Sets: {exercise.sets}</p>
-                    <button type='button' className='increaseSets' id='moreSets'>+</button>
-                    <button type='button' className='decreaseSets' id='lessSets'>-</button>
+                    <button type='button' className='increaseSets' id='moreSets'  onClick={(e) => {
+                      e.preventDefault()
+                      changeSets(exercise.exercise_id, 1)
+                      setActivePage('ExerciseList')
+                    }}>+</button>
+                    <button type='button' className='decreaseSets' id='lessSets'  onClick={(e) => {
+                      e.preventDefault()
+                      changeSets(exercise.exercise_id, -1)
+                      setActivePage('ExerciseList')
+                    }}>-</button>
                   </div>
                   
                   <div className='exerciseEdit'>
                     <p className='exerciseReps'>Reps: {exercise.reps}</p>
-                      <button type='button' className='increaseReps' id='moreReps'>+</button>
-                      <button type='button' className='decreaseReps' id='lessReps'>-</button>
+                      <button type='button' className='increaseReps' id='moreReps'  onClick={(e) => {
+                      e.preventDefault()
+                      changeReps(exercise.exercise_id, 1)
+                      setActivePage('ExerciseList')
+                    }}>+</button>
+                      <button type='button' className='decreaseReps' id='lessReps'   onClick={(e) => {
+                      e.preventDefault()
+                      changeReps(exercise.exercise_id, -1)
+                      setActivePage('ExerciseList')
+                    }}>-</button>
                   </div>
                   
                   <p className='exerciseDescription'>{exercise.description}</p>
