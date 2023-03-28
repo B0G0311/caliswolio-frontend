d7fe3b4b4c80861a4448f40926b0783d3cc18e24
Tyler Walston <tdwalston@my.waketech.edu>
2023-03-28 16:35:49 -0400

    Update to Sign In page
    Updated layout and design of sign-in page

--- src/components/signIn.jsx
+++ src/components/signIn.jsx
@@ -9,22 +9,20 @@ function SignIn()
 
   return(
     <div className='sign_in'>
-        <div className = "Swole photo">
+        <div className = "Swole_photo">
           <section>
             <img src="https://cdn.shopify.com/s/files/1/0160/2840/1712/products/swole_stewie-min-fdyc.png?v=1631575412" alt={"Stewie"} id="Swole" width="300" height="300"></img>
           </section>
         </div>
         
         <div className = "button-group">
-          <div className='wrap'>
+          <div className='login_button'>
             <button onClick={() => setActivePage('Login')} type="button" id="Login" className='Sign-in_Button'> Login</button>
           </div>
-          <br></br>
-          <div className='wrap'>     
+          <div className='register_button'>     
             <button onClick={() => setActivePage('Register')} type="button" id ="Register" className='Sign-in_Button'> Register</button>
           </div> 
-          <br></br>
-          <div className='wrap'> 
+          <div className='guest_button'> 
             <button onClick={() => setActivePage('Terms') } className='Sign-in_Button'>Use as guest</button>
           </div>
         </div>   
