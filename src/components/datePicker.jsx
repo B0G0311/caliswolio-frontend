7ec0401b3ae54956e60527ec32011530e8b1e7e6
sadavi <sadavis12@my.waketech.edu>
2023-04-08 20:24:31 -0400

    More changes to the exercise list

--- src/components/datePicker.jsx
+++ src/components/datePicker.jsx
@@ -1,28 +1,22 @@
 // The follow function creates a datePicker for the user, and then returns the day, month and year
 // selected as a date object variable. The variable will then be passed into a function.
 
-import React, { useState, useContext } from "react";
+import React, { useContext } from "react";
 import DatePicker from "react-datepicker";
 import WorkoutContext from "../context/workoutContext";
 import "react-datepicker/dist/react-datepicker.css";
+import "../css/exerciseList.css"
 
 function PickDate() {
     const { date, setDate } = useContext(WorkoutContext);
-    const [show, setShow] = useState(false);
     
     const onChange = (date) => {
         setDate(date);
-        setShow(false);
-    };
-    
-    const showDatePicker = () => {
-        setShow(true);
     };
     
     return (
         <div>
-        <button onClick={showDatePicker}>Select Date</button>
-        {show && (
+        {(
             <div>
             <DatePicker
                 selected={date}
@@ -31,9 +25,7 @@ function PickDate() {
             />
             </div>
         )}
-        <p>
-            {date.getMonth() + 1}-{date.getDate()}-{date.getFullYear()}
-        </p> 
+        
         </div>
     );
 }
