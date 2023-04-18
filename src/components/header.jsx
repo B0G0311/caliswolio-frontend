8ebe961c76bfd627a67a05828d9b63569f25ba9d
mtmears <mmears9795@gmail.com>
2023-04-18 15:35:26 -0400

    Fixed HREF and added logout

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,17 +2,39 @@ import React from "react";
 import { useContext } from 'react';
 import WorkoutContext from '../context/workoutContext';
 
-
 export default function Header() { 
     
-    const { setActivePage, isMember } = useContext(WorkoutContext)
-    return (
+    const { setIsMember, setActivePage, setPriorWorkoutListIsLoaded, setWorkoutQueueListIsLoaded, setTemplateWorkoutListIsLoaded, setUser, setSignInData, isMember } = useContext(WorkoutContext)
 
+    function figureOutActivePage() {
+        if (isMember) {
+            setPriorWorkoutListIsLoaded(false)
+            setWorkoutQueueListIsLoaded(false)
+            setTemplateWorkoutListIsLoaded(false)
+            setActivePage('MemberAccount')
+        } else {
+            setActivePage('SignIn')
+        }
+    }
+
+    function logout() {
+        setUser({})
+        setIsMember(false)
+        setSignInData(
+            {
+                email: '',
+                password: ''
+            }
+        )
+        setActivePage('SignIn')
+    }
+
+    return (
         <section id="navigation">
             <div className="container-fluid">
                 {/* Nav Bar */}
                 <nav className="navbar navbar-expand-lg navbar-dark navigation-head ">
-                    <a className="navbar-brand title-brand" onClick={() => setActivePage('SignIn')}>CaliSwolio</a>
+                    <a className="navbar-brand title-brand" href='/#' onClick={() => figureOutActivePage()}>CaliSwolio</a>
                     
 
                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggler01" aria-controls="navbarToggler01" aria-expanded="false" aria-label="Toggle navigation">
@@ -23,13 +45,16 @@ export default function Header() {
 
                         <ul className="navbar-nav ml-auto">
                             <li className="nav-item">
-                                <a className="nav-link"  id="ContactUs" onClick={() => setActivePage('ContactUs')}>Contact Us</a>
+                                <a className="nav-link" href='/#'  id="ContactUs" onClick={() => setActivePage('ContactUs')}>Contact Us</a>
                             </li>
                             <li className="nav-item">
-                                {(!isMember) && ( <a className="nav-link" onClick={() => setActivePage('Register')}   id ="Register">Register</a>)}
+                                {(!isMember) && ( <a className="nav-link" href='/#' onClick={() => setActivePage('Register')}   id ="Register">Register</a>)}
                             </li>
                             <li className="nav-item">
-                                {(!isMember) && (<a className="nav-link" onClick={() => setActivePage('Login') } id="Login">Log In</a>)}
+                                {(!isMember) && (<a className="nav-link" href='/#' onClick={() => setActivePage('Login') } id="Login">Log In</a>)}
+                            </li>
+                            <li  className="nav-item">
+                                {(isMember) && (<a className="nav-link" href='/#' onClick={() => logout() } id="Logout">Log Out</a>)}
                             </li>
                         </ul>
                     </div>
